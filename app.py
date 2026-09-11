dab67c6eef530743ac28d359f4c098538c657e74
ADITYA2428IT <aditya.2428it1599@kiet.edu>
2026-09-11 15:59:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"""
-    <h1>DevOps HA Web Application</h1>
-    <h2>Application is Running ✅</h2>
-    <p>Server: {socket.gethostname()}</p>
-    """
+    return """
+<h1>DevOps HA Web Application</h1>
+<h2>CI/CD Deployment Successful 🚀</h2>
+<p>Server: {}</p>
+""".format(socket.gethostname())
+   
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
